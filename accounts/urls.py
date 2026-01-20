e3513dbd58e7b7f6a46139881e201bc598112b72
Junayet Islam <junayetislam540@gmail.com>
2026-01-20 21:49:01 +0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'), # নতুন ড্যাশবোর্ড লিঙ্ক
     path('donors/', views.donor_list, name='donor_list'),
 ]
